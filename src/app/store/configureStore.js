dbd20e6e4c074fbc9c688c6130f30e6cbbcb3810
mcquam <murray.mcquarrie@gmail.com>
2020-04-14 15:58:40 +0100

    forms complete

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -2,7 +2,7 @@ import { createStore } from "redux";
 import rootReducer from "../reducers/rootReducer";
 
 export const configureStore = () => {
-    const store = createStore(rootReducer)
+    const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
     return store;
 }
